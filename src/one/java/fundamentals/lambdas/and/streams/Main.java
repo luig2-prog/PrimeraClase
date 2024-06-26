f17c66a46f2b604140916387f28527acb2085fc4
Luis Manuel Hernandez Jimenez <luisjimenezh8@gmail.com>
2024-06-26 12:34:49 -0500

    ✨ Add clases to Iterators, Collections, File Manager and lambdas

---
+++ src/one/java/fundamentals/lambdas/and/streams/Main.java
@@ -0,0 +1,29 @@
+package one.java.fundamentals.lambdas.and.streams;
+
+import one.java.fundamentals.clases.and.object.Persona;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 2, 2);
+
+        List<Integer> numerosDos = numeros.stream().filter(n -> n > 2.9).toList();
+
+        System.out.println(numerosDos);
+        numerosDos.forEach(numero -> System.out.println(numero));
+
+
+        List<String> palabras = Arrays.asList("Caminar", "Correr", "Saltar", "Caminar");
+
+        List<String> palabrasResult = palabras.stream().filter(p -> p.equalsIgnoreCase("caminar")).toList();
+
+        System.out.println(palabrasResult);
+
+
+
+    }
+
+}
